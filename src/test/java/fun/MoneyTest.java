f625bac7c171766b49ad9fa79b5ccd00d8da4819
bsjoberg <brian.sjoberg@excella.com>
2020-01-21 15:45:52 -0500

    Minimal code for testing equality.

--- src/test/java/fun/MoneyTest.java
+++ src/test/java/fun/MoneyTest.java
@@ -2,13 +2,21 @@ package fun;
 
 import org.junit.Test;
 
-import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.*;
 
 public class MoneyTest {
     @Test
     public void testMultiplication() {
         Dollar five = new Dollar(5);
-        five.times(2);
-        assertEquals(10, five.amount);
+        Dollar product = five.times(2);
+        assertEquals(10, product.amount);
+        product = five.times(3);
+        assertEquals(15, product.amount);
+    }
+
+    @Test
+    public void testEquality() {
+        Dollar five = new Dollar(5);
+        assertTrue(five.equals(new Dollar(5)));
     }
 }
